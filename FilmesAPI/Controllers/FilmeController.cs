d3c5e6dd27a86d285a67fc1130d35f67f4cfc994
Samih Freire <samihcisne@gmail.com>
2022-05-11 21:48:14 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{ 
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost] //Verbo de criação
+        public void AdionaFilme([FromBody] Filme filme) // [FROMBODY] indica que o filme vem atraves do corpo da requisição
+        {
+            filmes.Add(filme);
+        }
+    }
+}
